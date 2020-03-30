dfec4a1993132edb181f4be2a5bb1179943867b5
gdashua <gdashua@gmail.com>
2020-03-30 22:43:10 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+var http = require('http');
+var fs = require('fs');
+var route = require('url');
+var qs = require('querystring');
+
+function handleServer(req, res){
+	var path =route.parse(req.url, true);
+	var query= path.query;
+	if (req.url === '/'){
+		res.writeHead(200, {"Content-Type": "text/html"});
+		fs.createReadStream('./myForm.html').pipe(res);
+	}else if(path.pathname === './message' && req.method === 'POST'){
+		var rawData='';
+
+		req.on('data', data=> rawData +=data).on('end', ()=>{
+			var info = qs.parse(rawData);
+			res.end('Message: ${info.message}'})
+		}else{
+			res.writeHead(404, {"Content-Type": "text/plain"});
+			res.end('the requested page could not be found');
+		}
+	}
+	
+	http.createServer(handleServer).listen(3000);
+	console.log('Server is running on port 3000...');	
