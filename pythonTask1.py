941968550ed8117459a4023387dbaf22782ef699
gdashua <gdashua@gmail.com>
2020-04-10 10:41:15 +0100

    Add files via upload

---
+++ pythonTask1.py
@@ -0,0 +1,6 @@
+
+print("Please enter the radius of the circle to compute the Area:") 
+radius = float(input())
+Area= str(3.14*radius**2)
+
+print("The Area of this circle is "+Area+" unit square")
