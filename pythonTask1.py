99de4ee01417b06f6105583d2a4c6d7a63088b49
gdashua <gdashua@gmail.com>
2020-07-07 10:38:22 +0100

    Update pythonTask1.py

--- pythonTask1.py
+++ pythonTask1.py
@@ -1,6 +1,18 @@
 
-print("Please enter the radius of the circle to compute the Area:") 
-radius = float(input())
-Area= str(3.14*radius**2)
+#print("Please enter the radius of the circle to compute the Area:") 
+#radius = float(input())
+#Area= str(3.14*radius**2)
+#print("The Area of this circle is "+Area+" unit square")
 
-print("The Area of this circle is "+Area+" unit square")
+def basicCalc():
+  try:
+    print("please enter the expression you wish to evaluate below")
+    x = input()
+    x= str(eval(x))
+    print("The result is: "+x)
+    print()
+    basicCalc()
+  except:
+    print("this is a wrong input")
+    basicCalc()
+basicCalc()
